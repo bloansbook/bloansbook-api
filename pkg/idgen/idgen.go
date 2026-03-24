7ea32b749a464860d54c3804248b9f927a272b8f
PaulAjii <paulajii134@gmail.com>
2026-03-25 00:33:14 +0100

    feat: create id generation logic for the api

---
+++ pkg/idgen/idgen.go
@@ -0,0 +1,42 @@
+package idgen
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func GenerateSequentialID(ctx context.Context, db *pgxpool.Pool, table, column, prefix string) (string, error) {
+	var lastID string
+
+	query := fmt.Sprintf("SELECT %s FROM %s ORDER BY created_at DESC LIMIT 1", column, table)
+
+	err := db.QueryRow(ctx, query).Scan(&lastID)
+	if err != nil {
+		return fmt.Sprintf("%s-%4d", prefix, 1), nil
+	}
+
+	var num int
+	fmt.Sscanf(lastID, prefix+"-%4d", &num)
+	return fmt.Sprintf("%s-%4d", prefix, num+1), nil
+}
+
+func GenerateDailyID(ctx context.Context, db *pgxpool.Pool, table, column, prefix string) (string, error) {
+	today := time.Now().UTC()
+	dateStr := today.Format("2006/01/02")
+
+	var count int
+	query := fmt.Sprintf(
+		`SELECT COUNT(*) FROM %s WHERE DATE(created_at) = CURRENT_DATE`,
+		table,
+	)
+
+	err := db.QueryRow(ctx, query).Scan(&count)
+	if err != nil {
+		count = 0
+	}
+
+	return fmt.Sprintf("%s/%s/%03d", prefix, dateStr, count+1), nil
+}
