641871a72cfa7b414889b6dae234e50da1619edf
PaulAjii <paulajii134@gmail.com>
2026-03-31 16:34:48 +0100

    feat: add base model structures with ID, createdAt, and updatedAt fields

---
+++ internal/models/base.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type BaseWithId struct {
+	ID uuid.UUID `json:"id" db:"id"`
+}
+
+type BaseWithCreatedAt struct {
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+}
+
+type BaseWithUpdatedAt struct {
+	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
+}
+
+type BaseModel struct {
+	BaseWithId
+	BaseWithCreatedAt
+	BaseWithUpdatedAt
+}
+
+type BaseModelWithoutUpdatedAt struct {
+	BaseWithId
+	BaseWithCreatedAt
+}
