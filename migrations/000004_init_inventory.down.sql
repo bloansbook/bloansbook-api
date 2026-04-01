7399faff98e4e585ffa5043cbe1c83709304edc7
PaulAjii <paulajii134@gmail.com>
2026-04-01 23:24:58 +0100

    feat: add sales migration files for orders, order lines, invoices, and customer payments

--- migrations/000004_init_inventory.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS inventory_transactions;
-DROP TABLE IF EXISTS wac_snapshots;
---
+++ migrations/000004_init_sales.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS orders_lines;
+DROP TABLE IF EXISTS invoices;
+DROP TABLE IF EXISTS customer_payments;
