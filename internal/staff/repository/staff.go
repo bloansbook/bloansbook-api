469036cdfaf035e0c0fdf9294fb3d45f78d668ca
PaulAjii <paulajii134@gmail.com>
2026-05-08 00:47:45 +0100

    feat: add supabase_uid to CreateStaffPayload and update staff retrieval methods

--- internal/staff/repository/staff.go
+++ internal/staff/repository/staff.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/bloansbook/bloansbook-api/internal/models/roles"
 	"github.com/bloansbook/bloansbook-api/internal/models/staff"
 	"github.com/bloansbook/bloansbook-api/pkg/config"
 	"github.com/google/uuid"
@@ -46,9 +47,9 @@ func (s *StaffRepository) CountStaff(ctx context.Context) (int, error) {
 func (s *StaffRepository) CreateStaff(ctx context.Context, createdBy uuid.UUID, payload *staff.CreateStaffPayload, credentials staff.Credentials) (*staff.StaffCreate, error) {
 	stmt := `
 		INSERT INTO staff (
-			staff_id, password_hash, first_name, last_name, email, phone, address, date_of_birth, date_of_hire, emergency_contact_name, emergency_contact_phone, bank_name, bank_account_number, bank_account_name, department, job_title, pay_type, base_salary, status, created_by, has_login
+			staff_id, password_hash, first_name, last_name, email, phone, address, date_of_birth, date_of_hire, emergency_contact_name, emergency_contact_phone, bank_name, bank_account_number, bank_account_name, department, job_title, pay_type, base_salary, status, created_by, supabase_uid, has_login
 		) VALUES (
-			@staff_id, @password_hash, @first_name, @last_name, @email, @phone, @address, @date_of_birth, @date_of_hire, @emergency_contact_name, @emergency_contact_phone, @bank_name, @bank_account_number, @bank_account_name, @department, @job_title, @pay_type, @base_salary, @status, @created_by, @has_login
+			@staff_id, @password_hash, @first_name, @last_name, @email, @phone, @address, @date_of_birth, @date_of_hire, @emergency_contact_name, @emergency_contact_phone, @bank_name, @bank_account_number, @bank_account_name, @department, @job_title, @pay_type, @base_salary, @status, @created_by, @supabase_uid, @has_login
 		) RETURNING id, staff_id, first_name, last_name, email, phone, department, job_title, created_at, updated_at, status
 	`
 
@@ -84,6 +85,7 @@ func (s *StaffRepository) CreateStaff(ctx context.Context, createdBy uuid.UUID, 
 		"base_salary":             payload.BaseSalary,
 		"status":                  payload.Status,
 		"created_by":              createdBy,
+		"supabase_uid":            &payload.SupabaseUID,
 		"has_login":               payload.HasLogin,
 	})
 
@@ -163,7 +165,7 @@ func (s *StaffRepository) GetStaffByID(ctx context.Context, id uuid.UUID) (*staf
 			s.id, s.staff_id, s.password_hash, s.first_name, s.last_name, s.email, s.phone, s.address, s.date_of_birth, s.date_of_hire,
 			s.emergency_contact_name, s.emergency_contact_phone, s.bank_name, s.bank_account_number,
 			s.bank_account_name, s.department, s.job_title, s.pay_type, s.base_salary, s.status, s.fired_at, s.has_login,
-			s.superbase_uid, s.created_at, s.updated_at, creator.id AS creator_id, creator.staff_id AS creator_staff_id, creator.first_name AS creator_first_name, creator.last_name AS creator_last_name, creator.email AS creator_email, creator.phone AS creator_phone, creator.department AS creator_department, creator.job_title AS creator_job_title, creator.status AS creator_status
+			s.supabase_uid, s.created_at, s.updated_at, creator.id AS creator_id, creator.staff_id AS creator_staff_id, creator.first_name AS creator_first_name, creator.last_name AS creator_last_name, creator.email AS creator_email, creator.phone AS creator_phone, creator.department AS creator_department, creator.job_title AS creator_job_title, creator.status AS creator_status
 		FROM staff s
 		LEFT JOIN staff creator ON s.created_by = creator.id
 		WHERE s.id = $1
@@ -182,11 +184,16 @@ func (s *StaffRepository) GetStaffByID(ctx context.Context, id uuid.UUID) (*staf
 	return &staff, nil
 }
 
-func (s *StaffRepository) GetStaffByStaffID(ctx context.Context, staffID string) (*staff.StaffCreate, error) {
+func (s *StaffRepository) GetStaffByStaffID(ctx context.Context, staffID string) (*staff.Staff, error) {
 	stmt := `
-		SELECT id, staff_id, first_name, last_name, email, phone, department, job_title, created_at, updated_at
-		FROM staff
-		WHERE staff_id = $1
+		SELECT
+			s.id, s.staff_id, s.password_hash, s.first_name, s.last_name, s.email, s.phone, s.address, s.date_of_birth, s.date_of_hire,
+			s.emergency_contact_name, s.emergency_contact_phone, s.bank_name, s.bank_account_number,
+			s.bank_account_name, s.department, s.job_title, s.pay_type, s.base_salary, s.status, s.fired_at, s.has_login,
+			s.supabase_uid, s.created_at, s.updated_at, creator.id AS creator_id, creator.staff_id AS creator_staff_id, creator.first_name AS creator_first_name, creator.last_name AS creator_last_name, creator.email AS creator_email, creator.phone AS creator_phone, creator.department AS creator_department, creator.job_title AS creator_job_title, creator.status AS creator_status
+		FROM staff s
+		LEFT JOIN staff creator ON s.created_by = creator.id
+		WHERE s.staff_id = $1
 	`
 
 	rows, err := s.db.Query(ctx, stmt, staffID)
@@ -194,7 +201,7 @@ func (s *StaffRepository) GetStaffByStaffID(ctx context.Context, staffID string)
 		return nil, fmt.Errorf("failed to execute query for getting staff by staff_id: %w", err)
 	}
 
-	staff, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[staff.StaffCreate])
+	staff, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[staff.Staff])
 	if err != nil {
 		return nil, fmt.Errorf("failed to find staff by staff_id, not found: %w", err)
 	}
@@ -209,7 +216,7 @@ func (s *StaffRepository) GetAllStaff(ctx context.Context, limit, offset int) ([
 			s.id, s.password_hash, s.staff_id, s.first_name, s.last_name, s.email, s.phone, s.address, s.date_of_birth, s.date_of_hire,
 			s.emergency_contact_name, s.emergency_contact_phone, s.bank_name, s.bank_account_number,
 			s.bank_account_name, s.department, s.job_title, s.pay_type, s.base_salary, s.status, s.fired_at,
-			s.has_login, s.superbase_uid, s.created_at, s.updated_at, creator.id AS creator_id, creator.staff_id AS creator_staff_id, creator.first_name AS creator_first_name, creator.last_name AS creator_last_name, creator.email AS creator_email, creator.phone AS creator_phone, creator.department AS creator_department, creator.job_title AS creator_job_title, creator.status AS creator_status
+			s.has_login, s.supabase_uid, s.created_at, s.updated_at, creator.id AS creator_id, creator.staff_id AS creator_staff_id, creator.first_name AS creator_first_name, creator.last_name AS creator_last_name, creator.email AS creator_email, creator.phone AS creator_phone, creator.department AS creator_department, creator.job_title AS creator_job_title, creator.status AS creator_status
 		FROM staff s
 		LEFT JOIN staff creator ON s.created_by = creator.id
 		ORDER BY created_at DESC
@@ -284,4 +291,59 @@ func (s *StaffRepository) UpdateStaff(ctx context.Context, id uuid.UUID, payload
 	}
 
 	return &updatedStaff, nil
+}
+
+func (s *StaffRepository) GetStaffRoles(ctx context.Context, id uuid.UUID) ([]roles.RoleSummary, error) {
+	stmt := `
+		SELECT r.id, r.name
+		FROM roles r
+		JOIN staff_roles sr ON r.id = sr.role_id
+		WHERE sr.staff_id = $1
+		ORDER BY r.name
+      `
+	rows, err := s.db.Query(ctx, stmt, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch staff roles: %w", err)
+	}
+	defer rows.Close()
+
+	var roleList []roles.RoleSummary
+	for rows.Next() {
+		var role roles.RoleSummary
+		err := rows.Scan(&role.ID, &role.Name)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan role: %w", err)
+		}
+		roleList = append(roleList, role)
+	}
+	return roleList, nil
+}
+
+func (s *StaffRepository) GetStaffPermissions(ctx context.Context, id uuid.UUID) ([]roles.PermissionSummary, error) {
+	stmt := `
+		SELECT DISTINCT p.id, p.code, p.module
+		FROM permissions p
+		JOIN role_permissions rp ON p.id = rp.permission_id
+		JOIN staff_roles sr ON rp.role_id = sr.role_id
+		WHERE sr.staff_id = $1
+		ORDER BY p.module, p.code
+      `
+
+	rows, err := s.db.Query(ctx, stmt, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch staff permissions: %w", err)
+	}
+
+	defer rows.Close()
+
+	var permList []roles.PermissionSummary
+	for rows.Next() {
+		var perm roles.PermissionSummary
+		err := rows.Scan(&perm.ID, &perm.Code, &perm.Module)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan permission: %w", err)
+		}
+		permList = append(permList, perm)
+	}
+	return permList, nil
 }
