f2f0e248de0a475c9613de3c1f45db8557f08a5e
PaulAjii <paulajii134@gmail.com>
2026-05-08 00:40:48 +0100

    feat: update LoginResponse structure to include roles and adjust staff type

--- internal/auth/dtos.go
+++ internal/auth/dtos.go
@@ -1,12 +1,23 @@
 package auth
 
+import (
+	"github.com/bloansbook/bloansbook-api/internal/models/staff"
+)
+
 type LoginDTO struct {
 	StaffID  string `json:"staffId" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
 
 type LoginResponse struct {
-	AccessToken string   `json:"accessToken"`
-	Staff       string   `json:"staff"`
-	Permissions []string `json:"permissions"`
+	AccessToken string             `json:"accessToken"`
+	Staff       staff.StaffSummary `json:"staff"`
+	Roles       []string           `json:"roles"`
 }
+
+// TODO: Setup Login with staffID and Password with supabase auth
+// TODO: Return Response which includes the token - should save the role as part of the payload
+// TODO: Check the payload to see know role and role_permission
+// TODO: Authorise based on the role and permissions
+
+// TODO: Setup resend to send email to newly added users, their password and staffID so that they can login
