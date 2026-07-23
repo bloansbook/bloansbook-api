e043f036793cc5cc8efbc97acd272b8c9e14cf72
PaulAjii <paulajii134@gmail.com>
2026-07-23 14:42:14 +0100

    feat(auth/usecase): refactor Login and RefreshToken methods to return Tokens struct

--- internal/auth/usecase/usecase.go
+++ internal/auth/usecase/usecase.go
@@ -6,10 +6,17 @@ import (
 
 	"github.com/bloansbook/bloansbook-api/internal/auth"
 	"github.com/bloansbook/bloansbook-api/internal/auth/repository"
-	"github.com/bloansbook/bloansbook-api/internal/models/staff"
 	sr "github.com/bloansbook/bloansbook-api/internal/staff/repository"
 	"github.com/bloansbook/bloansbook-api/internal/staff/usecase"
+	"github.com/google/uuid"
 )
+
+type Tokens struct {
+	AccessToken  string
+	RefreshToken string
+	ExpiresIn    int
+	ExpiresAt    int64
+}
 
 type AuthUsecase struct {
 	authRepo     *repository.AuthRepository
@@ -25,25 +32,48 @@ func NewAuthUsecase(ar *repository.AuthRepository, sr *sr.StaffRepository, su *u
 	}
 }
 
-func (u *AuthUsecase) Login(ctx context.Context, payload auth.LoginDTO) (*staff.StaffDTO, string, error) {
+func (u *AuthUsecase) Login(ctx context.Context, payload auth.LoginDTO) (*Tokens, error) {
 	staff, err := u.staffRepo.GetStaffByStaffID(ctx, payload.StaffID)
 	if err != nil {
-		return nil, "", fmt.Errorf("Staff not found: %w", err)
+		return nil, fmt.Errorf("Staff not found: %w", err)
 	}
 
 	if !staff.HasLogin {
-		return nil, "", fmt.Errorf("Staff has no login access: %w", err)
+		return nil, fmt.Errorf("Staff has no login access: %w", err)
 	}
 
 	tokenResp, err := u.authRepo.SignInWithPassword(ctx, payload)
 	if err != nil {
-		return nil, "", fmt.Errorf("Invalid credentials: %w", err)
+		return nil, fmt.Errorf("Invalid credentials: %w", err)
 	}
 
-	staffDTO, err := u.staffUsecase.GetStaffByID(ctx, staff.ID)
+	return &Tokens{
+		AccessToken:  tokenResp.AccessToken,
+		RefreshToken: tokenResp.RefreshToken,
+		ExpiresIn:    tokenResp.ExpiresIn,
+		ExpiresAt:    tokenResp.ExpiresAt,
+	}, nil
+}
+
+func (u *AuthUsecase) RefreshToken(ctx context.Context, refreshToken string) (*Tokens, error) {
+	tokenResp, err := u.authRepo.RefreshToken(ctx, refreshToken)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to fetch staff: %w", err)
+		return nil, fmt.Errorf("%w", err)
 	}
 
-	return staffDTO, tokenResp.AccessToken, nil
+	return &Tokens{
+		AccessToken:  tokenResp.AccessToken,
+		RefreshToken: tokenResp.RefreshToken,
+		ExpiresIn:    tokenResp.ExpiresIn,
+		ExpiresAt:    tokenResp.ExpiresAt,
+	}, nil
+}
+
+func (u *AuthUsecase) GetProfile(ctx context.Context, userID uuid.UUID) (*auth.ProfileDTO, error) {
+	me, err := u.authRepo.GetMe(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return me, nil
 }
